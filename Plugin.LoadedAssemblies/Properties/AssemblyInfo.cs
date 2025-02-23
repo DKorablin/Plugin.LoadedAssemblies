b91cc3efeaff789e36ca1c1c1ae166c492c3d83a
DKorablin2 <danila.korablin@unity3d.com>
2025-02-23 16:52:09 +0300

    Unify plugins build

--- Plugin.LoadedAssemblies/Properties/AssemblyInfo.cs
+++ Plugin.LoadedAssemblies/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("8952337f-4c04-469e-908a-bf9fa1d3bdbd")]
 [assembly: System.CLSCompliant(true)]
 
@@ -9,14 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ProjectUrl", "https://github.com/DKorablin/Plugin.LoadedAssemblies")]
 #else
 
-[assembly: AssemblyTitle("Plugin.LoadedAssemblies")]
 [assembly: AssemblyDescription("View loaded assemblies and libraries in the current process")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("Plugin.LoadedAssemblies")]
-[assembly: AssemblyCopyright("Copyright © Danila Korablin 2016-2024")]
+[assembly: AssemblyCopyright("Copyright © Danila Korablin 2016-2025")]
 #endif
